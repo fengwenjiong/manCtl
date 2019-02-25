25bc27e505baa5275825e1edfe5d5b3cf188ebee
fengwenjiong <fengwenjiong@roborock.com>
2019-02-25 18:30:16 +0800

    cmd写死

--- manCtl.sh
+++ manCtl.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-#cmd_array=( AA 55 0D 0B 00 00 02 58 00 00 00 00 00 00 00 01 )  
 while [ "1" = "1" ]
 do
     read -p "enter direction:" direction
     if [ "$direction" = "q" ];then
       exit 0
     elif [ "$direction" = "1" ];then
-      #cmd_array=(AA 55 0D 0B 00 00 02 58 00 00 00 00 00 00 00 01)
-      for cmd_array in ${cmd_array[@]}
-      do
-        crc ^= ${cmd_array[@]}
-      done
-      echo AA 55 0D 0B 00 00 02 58 00 00 00 00 00 00 00 01 crc > /dev/tty0
-      #echo "UP"
+      echo AA 55 0D 0B 00 00 02 58 00 00 00 00 00 00 00 01 5D > /dev/tty0
+      echo "UP"
     elif [ "$direction" = "2" ];then
+      echo AA 55 0D 0B FF FF FD A8 00 00 00 00 00 00 00 01 52 > /dev/tty0
       echo "DOWN"
     elif [ "$direction" = "3" ];then
+      echo AA 55 0D 0B 00 00 00 00 00 00 01 F4 00 00 00 00 F3 > /dev/tty0
       echo "LEFT"
     elif [ "$direction" = "4" ];then
+      echo AA 55 0D 0B 00 00 00 00 FF FF FE 0C 00 00 00 00 F4 > /dev/tty0
       echo "RIGHT"
+    elif [ "$direction" = "0" ];then
+      echo AA 55 0D 0B 00 00 00 00 00 00 00 00 00 00 00 00 06 > /dev/tty0
+      echo "STOP"
     else
       echo "wrong"
     fi
